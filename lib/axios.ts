f3ceb380354e214452b9f32fdc1cf4fc0847e36d
Te-De-CX <tunjiheritage@gmail.com>
2026-06-30 03:06:04 +0100

    done

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "@/store/auth-store";
 
-const API_URL = "https://dembank.pythonanywhere.com/" || "http://localhost:8000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
 
 const api = axios.create({
   baseURL: API_URL,
